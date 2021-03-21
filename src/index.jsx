6666158eef96019293a55643a7514db268f575e6
Albert Ferre <libraescorpio1982@gmail.com>
2021-03-21 16:29:07 +0900

    Created class for hello function

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,22 @@ import '../assets/stylesheets/application.scss';
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Hello = () => {
-  return <h2>Hello yelloooo</h2>
+// const Hello = (props) => {
+//   const { name } = props;
+//   return <h2>Hello yelloooo {name}</h2>;
+// };
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Hello extends React.Component {
+  render() {
+    const { name } = this.props;
+    return (
+      <h2>Hello yelloooo {name}</h2>
+      );
+  }
 }
 
+
 const root = document.querySelector("#root");
-ReactDOM.render(<Hello />,root);
+ReactDOM.render(<Hello name="Pedrito" />, root);
 
